d7ef197431a90e5f7f3e57a21e29b0d0e90a1777
roofWalker <roofwalker13@outlook.com>
2021-10-05 14:06:53 -0700

    Home Page, About Page, Link setup

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/LayoutComponent";
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Our Family">
+            <p>
+                We love to cook and drink. Bring our recipes home 
+                and see what we're always looking forward to eating.
+            </p>
+        </Layout>
+    );
+}
+
+export default AboutPage;
